bef27377316db7e8b3691f3de23e32dd52ee9f8a
Patryk Kiszczuk <pkiszczuk0906@gmail.com>
2024-11-12 10:13:39 +0100

    Counting average rating for movie and add it to movie dataframe.

--- movielens.py
+++ movielens.py
@@ -7,3 +7,11 @@ tags = pd.read_csv('ml-latest-small/tags.csv')
 tags['timestamp'] = pd.to_datetime(tags['timestamp'], unit='s')
 
 ratings['timestamp'] = pd.to_datetime(ratings['timestamp'], unit='s')
+
+ratings_avg = ratings.groupby('movieId')['rating'].agg(['count','mean'])
+
+print(ratings_avg)
+
+movies = pd.merge(movies, ratings_avg, on='movieId')
+
+print(movies.head())
