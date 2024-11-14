f08350b411e0952294eaa6b88563ba203342fef4
Patryk Kiszczuk <pkiszczuk0906@gmail.com>
2024-11-14 12:13:50 +0100

    Add functions for importing dataset and converting timestamp.

--- movielens.py
+++ movielens.py
@@ -1,17 +1,23 @@
 import pandas as pd
 
-movies = pd.read_csv('ml-latest-small/movies.csv')
-ratings = pd.read_csv('ml-latest-small/ratings.csv')
-tags = pd.read_csv('ml-latest-small/tags.csv')
+def import_dataset(src):
+    try:
+        return pd.read_csv(src)
+    except FileNotFoundError:
+        print('The ' +  src + ' file was not found.')
+    except pd.errors.EmptyDataError:
+        print('The ' + src + ' file is empty.')
+    except pd.errors.ParserError:
+        print('The ' + src + ' file: parser error.')
 
-tags['timestamp'] = pd.to_datetime(tags['timestamp'], unit='s')
+def convert_timestamp(dataFrame, column, timeUnit):
+    dataFrame[column] = pd.to_datetime(dataFrame[column], unit=timeUnit)
 
-ratings['timestamp'] = pd.to_datetime(ratings['timestamp'], unit='s')
-
-ratings_avg = ratings.groupby('movieId')['rating'].agg(['count','mean'])
-
-print(ratings_avg)
-
-movies = pd.merge(movies, ratings_avg, on='movieId')
-
-print(movies.head())
+if __name__ == "__main__":
+    movies = import_dataset('ml-latest-small/movies.csv')
+    ratings = import_dataset('ml-latest-small/ratings.csv')
+    tags = import_dataset('ml-latest-small/tags.csv')
+    convert_timestamp(tags, 'timestamp', 's')
+    convert_timestamp(ratings, 'timestamp', 's')
+    ratings_avg = ratings.groupby('movieId')['rating'].agg(['count','mean'])
+    movies = pd.merge(movies, ratings_avg, on='movieId')
