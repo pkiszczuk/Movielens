24a0ce63595d96fee0cfc38da467062f55c5aeea
Patryk Kiszczuk <pkiszczuk0906@gmail.com>
2024-11-19 09:20:41 +0100

    Add procedure to print top for each geners.

--- movielens.py
+++ movielens.py
@@ -13,6 +13,21 @@ def import_dataset(src):
 def convert_timestamp(dataFrame, column, timeUnit):
     dataFrame[column] = pd.to_datetime(dataFrame[column], unit=timeUnit)
 
+def get_top_each_geners(dataFrame, topCount):
+    unique_genres = []
+    for items in dataFrame.genres.unique():
+        unique_genres = unique_genres + items.split('|')
+    unique_genres = list(set(unique_genres))
+    
+    for genre in unique_genres:
+        if genre == '(no genres listed)':
+            continue
+        print(genre)
+        result = dataFrame.loc[dataFrame["genres"].str.contains(genre)]
+        result = result.loc[result["count"] > 10]
+        result = result.sort_values(by='mean', ascending=False)
+        print(result.head(topCount))
+
 if __name__ == "__main__":
     movies = import_dataset('ml-latest-small/movies.csv')
     ratings = import_dataset('ml-latest-small/ratings.csv')
@@ -21,3 +36,5 @@ if __name__ == "__main__":
     convert_timestamp(ratings, 'timestamp', 's')
     ratings_avg = ratings.groupby('movieId')['rating'].agg(['count','mean'])
     movies = pd.merge(movies, ratings_avg, on='movieId')
+    get_top_each_geners(movies, 5)
+
